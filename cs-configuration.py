9f61dea9e1a38c36f8fbd5b976ea383ae7758d40
mottimalka <117903589+mottimalka@users.noreply.github.com>
2023-12-19 15:01:09 +0200

    first commit

---
+++ cs-configuration.py
@@ -0,0 +1,37 @@
+import os
+from azure.communication.networktraversal import CommunicationRelayClient
+from azure.identity import DefaultAzureCredential
+from azure.communication.identity import CommunicationIdentityClient
+
+try:
+  print("Azure Communication Services - Access Relay Configuration  Quickstart")
+  # You can find your endpoint and access token from your resource in the Azure Portal
+  connection_str = "endpoint=https://moti-azure-cs.unitedstates.communication.azure.com/;accesskey=GoRCiQPvlOqPZXqC0u54oQpIwy8od/vj4n3fXKz9QHM+IMtp7qEzGxR+KnFyXxxowBzsGr9/QVETBxQkDAs/xg=="
+  endpoint = "https://moti-azure-cs.unitedstates.communication.azure.com"
+
+  # To use Azure Active Directory Authentication (DefaultAzureCredential) make sure to have
+  # AZURE_TENANT_ID, AZURE_CLIENT_ID and AZURE_CLIENT_SECRET as env variables.
+  # We also need Identity client to get a User Identifier
+  identity_client = CommunicationIdentityClient(endpoint, DefaultAzureCredential())
+  relay_client = CommunicationRelayClient(endpoint, DefaultAzureCredential())
+
+  #You can also authenticate using your connection string
+  identity_client = CommunicationIdentityClient.from_connection_string(connection_str)
+  relay_client = CommunicationRelayClient.from_connection_string(connection_str)
+
+  user = identity_client.create_user()
+  relay_configuration = relay_client.get_relay_configuration(user=user)
+
+  for iceServer in relay_configuration.ice_servers:
+      assert iceServer.username is not None
+      print('Username: ' + iceServer.username)
+
+      assert iceServer.credential is not None
+      print('Credential: ' + iceServer.credential)
+      
+      assert iceServer.urls is not None
+      for url in iceServer.urls:
+          print('Url:' + url)
+except Exception as ex:
+  print("Exception:")
+  print(ex)
