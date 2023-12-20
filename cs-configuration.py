78d0ead83be1612c3e0102de9c44ae31686aee61
mottimalka <117903589+mottimalka@users.noreply.github.com>
2023-12-20 09:23:54 +0200

    update cms file

--- cs-configuration.py
+++ cs-configuration.py
@@ -6,9 +6,10 @@ from azure.communication.identity import CommunicationIdentityClient
 try:
   print("Azure Communication Services - Access Relay Configuration  Quickstart")
   # You can find your endpoint and access token from your resource in the Azure Portal
-  
-  endpoint = "https://moti-azure-cs.unitedstates.communication.azure.com"
 
+  connection_str = "endpoint=ENDPOINT;accessKey=KEY"
+  endpoint = "https://<RESOURCE_NAME>.communication.azure.com"
+  
   # To use Azure Active Directory Authentication (DefaultAzureCredential) make sure to have
   # AZURE_TENANT_ID, AZURE_CLIENT_ID and AZURE_CLIENT_SECRET as env variables.
   # We also need Identity client to get a User Identifier
